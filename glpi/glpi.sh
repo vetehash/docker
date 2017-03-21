2fdd3b25b99a46f2785f4e8ddcaf6e2f17a10481
VHT <v.huart@syntesia.fr>
2017-03-21 18:43:35 +0100

    Ajout dossier GLPI en dur

--- glpi/glpi.sh
+++ glpi/glpi.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-if [ -d "/var/www/glpi" ];
-then
-	echo "GLPI is already installed"
-else
-	wget -P https://github.com/glpi-project/glpi/releases/download/9.1/glpi-9.1.tar.gz
-	tar -xzf glpi-9.1.tar.gz
-	rm -Rf glpi-9.1.tar.gz
-	chown -R www-data:www-data /var/www/glpi
-fi
-
-cat << EOF > /etc/apache2/sites-enabled/000-default.conf
+cat << EOF > /etc/apache2/sites-available/000-default.conf
 <VirtualHost *:80>
 	DocumentRoot /var/www/glpi
 	<Directory /var/www/glpi>
